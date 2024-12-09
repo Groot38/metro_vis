558152338f8cd336f86a0daed7868e7763cd2923
Tom7946 <tom.gaubert@etu.univ-grenoble-alpes.fr>
2024-12-09 11:01:30 +0100

    Ajout de demo

---
+++ src/demo.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+# Chargement des données
+data = pd.read_csv("../data/demo/BASE_TD_FILO_IRIS_2021_DISP.csv", sep=";", dtype={"IRIS": str})
+codes_insee = [ # Pour limiter sur la métropole
+    "38057", "38059", "38071", "38068", "38111", "38126", "38150", "38151", 
+    "38158", "38169", "38170", "38179", "38185", "38188", "38200", "38516", 
+    "38187", "38317", "38471", "38229", "38235", "38258", "38252", "38271", 
+    "38277", "38279", "38281", "38309", "38325", "38328", "38364", "38382", 
+    "38388", "38421", "38423", "38436", "38445", "38472", "38474", "38478", 
+    "38485", "38486", "38524", "38528", "38529", "38533", "38540", "38545", 
+    "38562"
+]
+data_metro = data[data["IRIS"].str[:5].isin(codes_insee)]
+
+# Chargement des contours
+gdf = gpd.read_file('../data/iris_contours/iris_contours.shp')
+back = gpd.read_file('../data/iris_contours/iris_contours.shp')
+# back_gre = back[back["CODE_IRIS"].str[:5].isin(codes_insee)]
+# back_gre.to_file("../data/iris_contours/iris_contours.shp")
+
+
+# Nettoyage et jointure
+gdf_new = gdf.merge(data_metro[["IRIS", "DISP_MED21"]], left_on="CODE_IRIS", right_on="IRIS", how="right")
+gdf_new['DISP_MED21'] = pd.to_numeric(gdf_new['DISP_MED21'], errors='coerce')
+
+fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+
+# Fond de carte gris
+back.plot(ax=ax, color='lightgrey', edgecolor='none')
+
+# Ajout des données et de la barre continue
+gdf_new.plot(
+    column='DISP_MED21',
+    ax=ax,
+    legend=True,
+    cmap='viridis',
+    legend_kwds={
+        'shrink': 0.7, 
+        'label': "Médiane de revenu (€)", 
+        'orientation': "vertical"
+    }
+)
+
+ax.set_title('Carte des Zones IRIS par Médiane de revenu en 2021')
+ax.set_axis_off()
+
+plt.show()
