e80d584feb005edbb8bd509cf6b812d94a3b1374
Groot38 <soheil.mecheloukh@etu.univ-grenoble-alpes.fr>
2025-01-28 14:13:59 +0100

    mdp

--- src/main.py
+++ src/main.py
@@ -3,14 +3,27 @@ import streamlit as st
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
 
+# Fonction de login
 def login():
+    # Demander le nom d'utilisateur et le mot de passe
+    username = st.text_input("Username", key="username")
+    password = st.text_input("Password", type="password", key="password")
+    
+    # Authentification simple (ici avec des identifiants prédéfinis)
     if st.button("Log in"):
-        st.session_state.logged_in = True
-        st.rerun()
+        if username == "luc" and password == "tom":  # Identifiants simples pour la démo
+            st.session_state.logged_in = True
+            st.success("Login successful!")
+            st.rerun()
+            
+        else:
+            st.error("Invalid username or password.")
 
+# Fonction de logout
 def logout():
     if st.button("Log out"):
         st.session_state.logged_in = False
+        st.success("Logged out successfully!")
         st.rerun()
 
 login_page = st.Page(login, title="Log in", icon="🔑")
